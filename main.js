f96b2587bf3bd7984fd5f880d7ae11d3cc3f4e7e
Karvannan Rajendiran <karvannan.r@itp.objectfrontier.com>
2018-09-25 20:08:02 +0530

    update electron package

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+    path = require('path');
+    url = require('url');
+    app.on('ready', function() {
+        console.log('Starting application!');
+        mainWindow = new BrowserWindow({ width: 1280, height: 960 });
+        
+        // Change loadUrl to load index.html
+        // using url and path package 
+        // to format the file url
+        mainWindow.loadURL(url.format({
+            //__dirname is the current working dir
+            pathname: path.join(__dirname, 'dist/ang-electron', 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    
+        // Opens dev tools
+        mainWindow.webContents.openDevTools();
+        mainWindow.on('closed', function() {
+            mainWindow = null;
+        });
+    });
+    app.on('window-all-closed', function() {
+        app.quit();
+    });
